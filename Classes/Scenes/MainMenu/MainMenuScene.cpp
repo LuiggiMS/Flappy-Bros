99aac067dd39360bac31e6df5e8068f08ce298e4
Luiggi Minaya <luiggi.minaya@gmail.com>
2019-09-25 01:03:37 -0500

    Agregado canción de fondo y nueva imagen de Bird Death y de botón play

--- Classes/Scenes/MainMenu/MainMenuScene.cpp
+++ Classes/Scenes/MainMenu/MainMenuScene.cpp
@@ -32,18 +32,21 @@ bool MainMenuScene::init()
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-    auto backgroundSprite = Sprite::create( "Background.png" );
+    auto backgroundSprite = Sprite::create( "Backgroun Main Scene.png" );
     backgroundSprite->setPosition( Point( visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y ) );
     
     this->addChild( backgroundSprite );
         
-    auto playItem = MenuItemImage::create( "Play Button.png", "Play Button Clicked.png", CC_CALLBACK_1( MainMenuScene::GoToGameScene, this ) );
+    auto playItem = MenuItemImage::create( "play_button.png", "play_button_clicked.png", CC_CALLBACK_1( MainMenuScene::GoToGameScene, this ) );
     playItem->setPosition( Point( visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y ) );
     
     auto menu = Menu::create( playItem, NULL );
     menu->setPosition( Point::ZERO );
     
     this->addChild( menu );
+    
+    CocosDenshion::SimpleAudioEngine::getInstance( )->stopBackgroundMusic();
+    CocosDenshion::SimpleAudioEngine::getInstance( )->playBackgroundMusic("Sounds/intro_main_scene.m4a");
     
     return true;
 }
